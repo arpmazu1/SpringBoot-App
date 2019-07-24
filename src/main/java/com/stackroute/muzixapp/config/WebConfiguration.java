520274d0ed2418432ed0c93946631f809f4e3ac7
Arpita <arpita.mazumder02@gmail.com>
2019-07-24 15:55:14 +0530

    Committing the fourth task.

--- src/main/java/com/stackroute/muzixapp/config/WebConfiguration.java
+++ src/main/java/com/stackroute/muzixapp/config/WebConfiguration.java
@@ -4,12 +4,15 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
 
 //configuration class
 @Configuration
@@ -22,15 +25,24 @@ public class WebConfiguration {
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
     }
-    //inorder to run the swagger2 to go to this link http://localhost:8080/v2/api-docs
+    //inorder to run the swagger2 to go to this link http://localhost:8080/swagger-ui.html
     //bean for swagger 2 docket product api
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
-                .paths(regex("/product.*"))
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.stackroute.muzixapp.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Track Management REST API")
+                .contact(new Contact("Arpita", "https://www.gmail.com", "arpita.mazumder02@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
                 .build();
-//                .apiInfo(metaData());
     }
 }
