8a3fcb79544065611b2a8770f4d9c6b1b7324343
Arpita <arpita.mazumder02@gmail.com>
2019-07-24 14:53:25 +0530

    Committing the fourth task.

--- src/main/java/com/stackroute/muzixapp/config/WebConfiguration.java
+++ src/main/java/com/stackroute/muzixapp/config/WebConfiguration.java
@@ -4,9 +4,16 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 //configuration class
 @Configuration
+@EnableSwagger2
 public class WebConfiguration {
     //bean for console (h2Servletregistration)
     @Bean
@@ -14,5 +21,16 @@ public class WebConfiguration {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
+    }
+    //inorder to run the swagger2 to go to this link http://localhost:8080/v2/api-docs
+    //bean for swagger 2 docket product api
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
+                .paths(regex("/product.*"))
+                .build();
+//                .apiInfo(metaData());
     }
 }
