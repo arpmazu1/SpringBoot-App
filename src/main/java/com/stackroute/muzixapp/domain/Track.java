7a1e9a6b2b7e98826d705489257dbdd707e7f489
Arpita <arpita.mazumder02@gmail.com>
2019-07-25 09:50:24 +0530

    Committing the ninth task.

--- src/main/java/com/stackroute/muzixapp/domain/Track.java
+++ src/main/java/com/stackroute/muzixapp/domain/Track.java
@@ -1,11 +1,20 @@
 package com.stackroute.muzixapp.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 //track class
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
 
         //variables
@@ -16,52 +25,6 @@ public class Track {
         @Column
         private String comment;
 
-
-        //constructor
-        public Track() {
-        }
-
-        public Track(int id, String name, String comment) {
-            this.id = id;
-            this.name = name;
-            this.comment = comment;
-        }
-
-        //getter and setters
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getComment() {
-            return comment;
-        }
-
-        public void setComment(String comment) {
-            this.comment = comment;
-        }
-
-
-        //to string mehod
-        @Override
-        public String toString() {
-            return "Track{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", comment='" + comment + '\'' +
-                    '}';
-        }
 
 
 
