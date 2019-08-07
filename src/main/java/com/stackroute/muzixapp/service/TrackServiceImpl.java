c3b548b6040cc05ebcf208fa16514d256611ffd9
arpmazu1 <51942878+arpmazu1@users.noreply.github.com>
2019-08-07 22:34:20 +0530

    Submission

--- src/main/java/com/stackroute/muzixapp/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/muzixapp/service/TrackServiceImpl.java
@@ -36,7 +36,13 @@ public class TrackServiceImpl implements TrackService {
         if (trackRepository.existsById(track.getId())){
             throw new TrackAlreadyExistsException("Track already exists");
         }
+        
+        try{
             Track savedTrack = trackRepository.save(track);
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+         }
         return savedTrack;
     }
 
