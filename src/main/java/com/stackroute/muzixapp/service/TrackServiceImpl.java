066bdc091faed15746d5b86ee9dc8aa218c8a4c5
arpmazu1 <51942878+arpmazu1@users.noreply.github.com>
2019-08-08 11:01:20 +0530

    Submission

--- src/main/java/com/stackroute/muzixapp/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/muzixapp/service/TrackServiceImpl.java
@@ -48,9 +48,13 @@ public class TrackServiceImpl implements TrackService {
 
     @Override
     public void deleteTrack(int id) {
-
-        trackRepository.deleteById(id);
-
+        Optional<Track> track = trackRepository.findById(id);
+        if(track.isPresent()) {
+            trackRepository.deleteById(id);
+            return track.get();
+        }   
+        else {
+            return null;
     }
 
     @Override
